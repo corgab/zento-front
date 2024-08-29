24492cbb2351aab05504f589316423472feef0e8
gab <corbanigabriele@gmail.com>
2024-08-30 00:06:39 +0200

    ADD: post component

---
+++ src/components/CardContent.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- LAYOUT DA AGGIUNGERE -->
+    <div>
+        <div v-for="img in post.images">
+            <img v-if="img.is_featured == 1" :src="img.link" alt="" class="img-fluid">
+            <small v-for="tag in post.tags">
+                {{ tag.name }}
+            </small>
+        </div>
+        <div v-if="post.featured">
+            <small>premium</small>
+        </div>
+        <h1>{{ post.title }}</h1>
+        <p class="inline-block text-truncate"></p>
+        <p>{{ post.reading_time }}</p>
+        <div class="d-flex">
+            <div>
+                <small>By {{ post.user_name }}</small>
+            </div>
+            <i class="bi bi-balloon"></i>
+            <div>
+                <p>{{ post.created_date }}</p>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardContent',
+    props: {
+        post: {
+            type: Object,
+            required: true
+        }
+
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,42 @@
 <template>
     <div>
         <h1>ciao</h1>
+        <CardContent v-for="post in posts" :key="post.id" :post="post"/>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import CardContent from "../components/CardContent.vue";
+import axios from 'axios'; // Importa axios
+
+export default {
+    components: {
+        CardContent
+    },
+    data() {
+        return {
+            posts: [], // Array per conservare i post
+            //   loading: true, // Flag per il caricamento
+        }
+    },
+    methods: {
+        fecthPosts() {
+            axios.get('http://127.0.0.1:8000/api/posts/', {
+                params: {
+                    per_page: 6
+                }
+            }
+            ).then(response => {
+                this.posts = response.data.data;
+                console.log(response.data.data)
+                console.log(this.posts)
+            })
+        }
+    },
+    mounted() {
+        this.fecthPosts()
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
