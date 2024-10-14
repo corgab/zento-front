4342730cbcb208d79fef58e4e0dc3314fd1f1dfd
gab <corbanigabriele@gmail.com>
2024-10-14 21:05:36 +0200

    url for production

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-  // appUrl: 'http://31.56.7.95/',
-  appUrl: 'http://127.0.0.1:8000/',
+  appUrl: 'http://31.56.7.95:3000/',
+  // appUrl: 'http://127.0.0.1:8000/',
   isAuthenticated: !!localStorage.getItem('authToken'),
   user: null,
 
