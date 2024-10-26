227e19ba83087721eec66938e04b3ac97e45bca7
gab <corbanigabriele@gmail.com>
2024-10-26 17:12:10 +0200

    .

--- src/store.js
+++ src/store.js
@@ -2,8 +2,7 @@ import { reactive } from 'vue'
 import axios from 'axios'
 
 export const store = reactive({
-  // appUrl: 'http://127.0.0.1:8000/',
-  appUrl: 'http://31.56.7.95:3000/',
+  appUrl: 'http://127.0.0.1:8000/',
   user: null,
 
   register(user) {
